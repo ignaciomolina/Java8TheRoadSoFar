1b83fe4b904295dbbd707d3f730213df1a943b96
Ignacio Molina Cuquerella <imolina@paradigmadigital.com>
2018-06-13 16:55:15 +0200

    add funcional interface for throwing actions and do refactor to TimeWaiter

--- src/main/java/com/paradigma/java8/utils/TimeWaiter.java
+++ src/main/java/com/paradigma/java8/utils/TimeWaiter.java
@@ -1,38 +1,40 @@
 package com.paradigma.java8.utils;
 
-import java.io.IOException;
 import java.time.Duration;
 
 import com.paradigma.java8.functional.Action;
+import com.paradigma.java8.functional.ThrowingAction;
 
 public class TimeWaiter {
 
   public static void waitFor(Duration duration) {
 
-    try {
-      Thread.sleep(duration.toMillis());
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
-    }
+    handleCheckedException(() -> Thread.sleep(duration.toMillis()));
   }
   
   public static void waitKey() {
-    try {
 
-      System.in.read();
-    } catch (IOException e) {
-
-      throw new RuntimeException(e);
-    }
+    handleCheckedException(() -> System.in.read());
   }
-  
+
   public static void doUntilKey(Action task) {
-    
-    try {
+
+    handleCheckedException(() -> {
+
       while (System.in.available() == 0) {
+
         task.execute();
       }
-    } catch (IOException e) {
+    });
+  }
+  
+  private static void handleCheckedException(ThrowingAction task) {
+
+    try {
+
+      task.tryToExecute();
+    } catch (Exception e) {
+
       throw new RuntimeException(e);
     }
   }
